fb853cf8b0e005724f68dbd61c1d58077735b534
jaimezzapata <zapataval2304@gmail.com>
2024-04-18 20:53:18 -0500

    Se hace metodo post

--- front/src/layouts/components/auth/Login.jsx
+++ front/src/layouts/components/auth/Login.jsx
@@ -1,16 +1,32 @@
 import './Login.css'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+let apiUsurios = 'http://localhost:5174/usuarios'
 
 const Login = () => {
     const [getUsuario, setUsuario] = useState("admin")
     const [getContrasena, setContrasena] = useState("")
+    const [getUsuarios, setUsuarios] = useState([])
     let redireccion = useNavigate()
-    if (getUsuario === 'Jaime') {
-        redireccion("/home")
+    async function traerUsuarios() {
+        let resultado = await axios.get(apiUsurios)
+        setUsuarios(resultado.data)
+        console.log(resultado.data)
+    }
+    function buscarUsuario() {
+        let estado = getUsuarios.some((usuario) => getUsuario === usuario.usuario)
+        return estado
+    }
+    function iniciarSesion() {
+        if (buscarUsuario()) {
+            redireccion("/home")
+        } else {
+            console.log('Error de credenciales')
+            traerUsuarios()
+        }
     }
     return (
-
         <div className="login-container">
             <h2>Iniciar Sesión</h2>
             <form>
@@ -22,7 +38,7 @@ const Login = () => {
                     <label>Contraseña</label>
                     <input onChange={(e) => { setContrasena(e.target.value) }} placeholder='Contraseña' type="text" />
                 </div>
-                <button type="button">Iniciar Sesión</button>
+                <button onClick={iniciarSesion} type="button">Iniciar Sesión</button>
             </form>
         </div>
     )
